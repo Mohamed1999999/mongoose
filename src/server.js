cd9c7d3bb4b94e3d75603d83ce96a10b50b2eb85
Mohamed1999999 <124585436+Mohamed1999999@users.noreply.github.com>
2023-08-28 13:19:10 +0000

    Update server.js
    j'ai ajouté le serveur et je l'ai connecter à notre base de données

--- src/server.js
+++ src/server.js
@@ -1,14 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
 
 // Middleware pour gérer les données JSON dans les requêtes
 app.use(express.json());
 
-// Middleware de journalisation simple
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 });
+
+// Connexion à la base de données MongoDB
+const uri = process.env.MONGO_URI;
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connecté à la base de données");
+  })
+  .catch((err) => {
+    console.error("Erreur de connexion à la base de données :", err);
+  });
 
 // Point d'entrée de base
 app.get("/", (req, res) => {
